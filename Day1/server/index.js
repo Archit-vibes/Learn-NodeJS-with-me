08bcde947d1741719bb25ad002274c6fe1246cbb
Archit Sharma <hiswagboy01@gmail.com>
2026-02-16 23:36:40 +0530

    Learnt Handelling URLs

--- Day1/server/index.js
+++ Day1/server/index.js
@@ -1,8 +1,23 @@
 const http = require("http")
+const url = require("url")
 
 const server = http.createServer((req , res) => {
-    console.log(req.rawHeaders)
-    res.end("Hello World from the server")
+
+    const myurl = url.parse(req.url , true)
+    
+    console.log("--------------------------" , myurl)
+    switch(myurl.pathname) {
+        case "/":
+            res.end("Welcome to the homepage")
+            break;
+
+            case "/about":
+                res.end("Welcome to the about page")
+                break;
+            default:
+                res.end("404 Not Found")
+                break;
+    }
 });
 
 server.listen(8000, () => {
